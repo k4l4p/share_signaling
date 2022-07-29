285c2606dd3ff719a32fac0303100eca6dac2c45
k4l4p <kalapwong@gmail.com>
2022-07-30 01:36:49 +0800

    simple exchange complete

--- index.js
+++ index.js
@@ -3,55 +3,92 @@ const express = require('express')
 const SocketServer = require('ws').Server
 const fs = require('fs');
 
-//指定開啟的 port
 const PORT = 8000
 
-//創建 express 的物件，並綁定及監聽 3000 port ，且設定開啟後在 console 中提示
 const server = express()
    .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
-//將 express 交給 SocketServer 開啟 WebSocket 的服務
 const wss = new SocketServer({ server })
 
-//當 WebSocket 從外部連結時執行
+let globalData = {}
+
+const read = () => {
+   let temp = fs.readFileSync(__dirname + "/" + "data.json", { encoding: 'utf8' })
+   globalData = JSON.parse(temp)
+   // fs.readFile(__dirname + "/" + "data.json", 'utf8', function (err, data) {
+   //    globalData = JSON.parse(data)
+   // })
+}
+
+const write = () => {
+   let data = JSON.stringify(globalData)
+   fs.writeFileSync(__dirname + "/" + "data.json", data)
+   // fs.writeFile(__dirname + "/" + "data.json", data, (err)=>{
+   //    if (err) {
+   //       throw err
+   //    }
+   // })
+}
+
 wss.on('connection', ws => {
-
-   //連結時執行此 console 提示
    console.log('Client connected')
-   fs.readFile(__dirname + "/" + "data.json", 'utf8', function (err, data) {
-      let temp = JSON.parse(data);
-      ws.send(data)
-
-   })
-
    ws.on('message', (data)=>{
       let temp = JSON.parse(data)
-      console.log(temp.phase)
       switch (temp.phase){
          case 'req_key':
             let key = Math.floor(Math.random() * (899) + 100)
             ws.key = key
-            let data = {}
-            data[key] = null
-            data = JSON.stringify(data)
-            fs.writeFile(__dirname + "/" + "data.json", data, (err)=>{
-               if (err) {
-                  throw err
-               }
-            })
+            read()
+            globalData[key] = {
+               in: null,
+               out: null
+            }
+            write()
+            ws.send(JSON.stringify({phase: 'req_key',key: key}))
+            console.log('key: '+key)
             break
          case 'start':
+            read()
+            globalData[ws.key].in = temp.ice
+            write()
             break
          case 'recv':
+            read()
+            if (temp?.ice) {
+               globalData[temp.key].out = temp.ice
+               wss.clients.forEach(c => {
+                  console.log(c.key, temp.key)
+                  if (c?.key === Number.parseInt(temp.key)){
+                     console.log('hi')
+                     c.send(JSON.stringify({
+                        phase: 'start',
+                        ice: temp.ice
+                     }))
+                  }
+               })
+
+
+            }else {
+               let temp_ice = globalData[temp.key].in
+               ws.send(JSON.stringify({
+                  phase: 'recv',
+                  ice: temp_ice
+               }))   
+            }
+            
             break
          case 'close':
-            
+            delete globalData[ws.key]
+            console.log('delete key')
             break
       }
    })
 
-   //當 WebSocket 的連線關閉時執行
    ws.on('close', () => {
+      read()
+      delete globalData[ws.key]
+      write()
+      console.log('delete key: ', ws.key)
       console.log('Close connected')
    })
 })
